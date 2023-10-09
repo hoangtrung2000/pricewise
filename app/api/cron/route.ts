4f0e4fb21b430c17a31bf7f517369648019409d7
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2023-10-10 01:49:14 +0700

    fix error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,6 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
